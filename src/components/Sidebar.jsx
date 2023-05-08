1cd7d0f3040fcbe2fbba7ec927ba07ce083ddda7
gideonalves <gideonam@yahoo.com.br>
2023-05-07 22:10:38 -0300

    apicando responsividade

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -10,8 +11,8 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1579403124614-197f69d8187b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDg4MDd8MHwxfHNlYXJjaHw4fHxkZXZ8ZW58MHx8fHwxNjgzMzMwOTEz&ixlib=rb-4.0.3&q=500&w=500" />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/66545644?v=4" />
-
+              
+                <Avatar src="https://avatars.githubusercontent.com/u/6643122?v=4"/>
                 <strong>Gideon Alves</strong>
                 <span>Web Developer</span>
             </div>
